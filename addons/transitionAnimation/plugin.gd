235bf58d88baa54fefb81c78c96eff08f52397bc
Hubert <tim7840639990414@hotmail.com>
2020-09-05 20:25:17 +0800

    fix sin range

--- addons/transitionAnimation/plugin.gd
+++ addons/transitionAnimation/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("TransitionAnimation", "Control", preload("transitionAnimation.gd"), get_editor_interface().get_base_control().get_icon("AnimatedSprite", "EditorIcons"))
+	add_custom_type("TransitionAnimation", "Control", preload("transitionAnimation.gd"), get_editor_interface().get_base_control().get_icon("Control", "EditorIcons"))
 
 
 func _exit_tree():
--- addons/transitionAnimation/sinGradient.gd
+++ addons/transitionAnimation/sinGradient.gd
@@ -3,4 +3,4 @@ class_name SinGradient
 extends "res://addons/transitionAnimation/transitionGradient.gd"
 
 func get_gradient(p_time:float) -> float:
-	return sin(p_time * PI - PI/2)
+	return (sin(p_time * PI - PI/2) + 1.0) / 2.0
