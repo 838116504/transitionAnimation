74c50f57ff3a18991dc97fe77b87cc1e54f584eb
Hubert <tim7840639990414@hotmail.com>
2020-09-07 21:50:59 +0800

    Add gradient preview

--- addons/transitionAnimation/plugin.gd
+++ addons/transitionAnimation/plugin.gd
@@ -1,10 +1,13 @@
 tool
 extends EditorPlugin
 
-
+var previewGen
 func _enter_tree():
 	add_custom_type("TransitionAnimation", "Control", preload("transitionAnimation.gd"), get_editor_interface().get_base_control().get_icon("Control", "EditorIcons"))
+	previewGen = preload("previewGenerator.gd").new()
+	get_editor_interface().get_resource_previewer().add_preview_generator(previewGen)
 
 
 func _exit_tree():
 	remove_custom_type("TransitionAnimation")
+	get_editor_interface().get_resource_previewer().remove_preview_generator(previewGen)
